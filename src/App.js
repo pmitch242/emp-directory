17769e787a85ace71cf8a87602368eeb59f6f484
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-14 21:33:09 -0400

    added Directory page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Directory from './pages/Directory';
 function App() {
   return (
     <div>
       <h1>Employee Directory Test</h1>
+      <Directory/>
     </div>
   );
 }
