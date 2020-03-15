ad5eb53c7428f89905ac751d4cdbe5e6675194ce
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-14 22:52:25 -0400

    added props

--- src/pages/Directory/index.js
+++ src/pages/Directory/index.js
@@ -1,2 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container, Row } from 'react-bootstrap'
 import './style.css';
+import EmpCard from '../../components/EmpCard';
+import employee from '../../employees.json'
+
+
+class Directory extends Component {
+    state ={
+        employee
+    }
+    render(){ 
+        return (
+        <div>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <EmpCard 
+                        id={employee[0].id}
+                        key={employee[0].id}
+                        name={employee[0].name}
+                        image={employee[0].image}
+                        occupation={employee[0].occupation}
+                        location={employee[0].location}
+                        funninessLevel={employee[0].funninessLevel}
+                    />
+                    <EmpCard />
+                    <EmpCard />
+                </Row>
+            </Container>
+        </div>
+    )}
+}
+export default Directory;
