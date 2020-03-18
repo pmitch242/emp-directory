8fce5b4461031dca0ee566be852f7dea1ab28d67
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-18 13:17:33 -0400

    restyled card

--- src/components/EmpCard/index.js
+++ src/components/EmpCard/index.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './style.css';
-import { Card, Col } from 'react-bootstrap'
 
 function EmpCard(props) {
     return (
-        <div>
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={props.image} />
-                    <Card.Body>
-                        <ul>
-                            <li>
-                                <strong>Name:</strong> {props.name}
-                            </li>
-                            <li>
-                                <strong>Occupation:</strong> {props.occupation}
-                            </li>
-                            <li>
-                                <strong>Location:</strong> {props.location}
-                            </li>
-                            <li>
-                                <strong>Funny Rate:</strong> {props.funninessLevel}
-                            </li>
-                        </ul>
-                    </Card.Body>
-                </Card>
-            </Col>
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name:</strong> {props.name}
+                    </li>
+                    <li>
+                        <strong>Occupation:</strong> {props.occupation}
+                    </li>
+                    <li>
+                        <strong>Location:</strong> {props.location}
+                    </li>
+                    <li>
+                        <strong>Funny Rate:</strong> {props.funninessLevel}
+                    </li>
+                </ul>
+            </div>
+            <span onClick={() => props.removeFriend(props.id)} className="remove">
+                𝘅
+          </span>
         </div>
     );
 }
