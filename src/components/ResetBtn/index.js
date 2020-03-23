94f2298f05071bfd4111c67cb65479d1441bfd37
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-23 13:01:17 -0400

    added function to handle sorting

---
+++ src/components/ResetBtn/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import Button from 'react-bootstrap/Button'
+import './style.css';
+
+class ResetBtn extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default ResetBtn;
--- src/pages/Directory/index.js
+++ src/pages/Directory/index.js
@@ -3,18 +3,74 @@ import { Container, Row } from 'react-bootstrap'
 import './style.css';
 import EmpCard from '../../components/EmpCard';
 import employees from '../../employees.json'
+import Button from 'react-bootstrap/Button'
+
 
 
 class Directory extends Component {
-    state = {
-        employees
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees,
+            filterEmp: employees
+        };
     }
+
+    handleClick = async event => {
+        if (event.target.innerText === 'All') {
+            await this.setState({
+                filterEmp: employees
+            });
+        } else {
+            let empCopy = employees.filter(employee => {
+                return employee.maturity === event.target.innerText
+            });
+
+            await this.setState({
+                filterEmp: empCopy
+            })
+        }
+    };
+
+    handleSort = async event => {
+        let sortedArray = this.state.filterEmp
+        console.log(sortedArray)
+        console.log(event.target.innerText)
+        if (event.target.innerText === 'Ascending') {
+            sortedArray.sort((a, b) => { return a.funninessLevel - b.funninessLevel });
+            console.log(sortedArray);
+            await this.setState({
+                filterEmp: sortedArray
+            });
+        } else if (event.target.innerText === 'Descending') {
+            sortedArray.sort((a, b) => { return b.funninessLevel - a.funninessLevel });
+            await this.setState({
+                filterEmp: sortedArray
+            });
+        } else {
+            sortedArray.sort((a, b) => { return a.id - b.id });
+            await this.setState({
+                filterEmp: sortedArray
+            });
+        }
+    }
+
     render() {
         return (
             <div>
+                <h5>Filter:</h5>
+                <Button onClick={this.handleClick} variant="danger">All</Button>{' '}
+                <Button onClick={this.handleClick} variant="danger">Adult</Button>{' '}
+                <Button onClick={this.handleClick} variant="danger">Child</Button>{' '}
+                <Button onClick={this.handleClick} variant="danger">God</Button>{' '}
+                <h5>Sort</h5>
+                <Button onClick={this.handleSort} variant="success">Ascending</Button>{' '}
+                <Button onClick={this.handleSort} variant="success">Descending</Button>{' '}
+                <Button onClick={this.handleSort} variant="success">Reset</Button>{' '}
+
                 <Container>
                     <Row className="justify-content-md-center">
-                        {this.state.employees.map(employee => (
+                        {this.state.filterEmp.map(employee => (
                             <EmpCard
                                 id={employee.id}
                                 key={employee.id}
